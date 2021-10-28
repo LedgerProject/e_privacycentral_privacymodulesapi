1c819932b81ab84fb5b40f2523d0029fa82e29d6
jacquarg <guillaume.jacquart@hoodbrains.com>
2021-10-28 08:39:37 +0200

    Update Trackers API draft

--- privacymodulesapi/src/main/java/foundation/e/privacymodules/trackers/IBlockTrackersPrivacyModule.kt
+++ privacymodulesapi/src/main/java/foundation/e/privacymodules/trackers/IBlockTrackersPrivacyModule.kt
@@ -6,6 +6,12 @@ package foundation.e.privacymodules.trackers
 interface IBlockTrackersPrivacyModule {
 
     /**
+     * Get the state of the blockin module
+     * @return true when blocking is enabled, false otherwise.
+     */
+    fun isBlockingEnabled(): Boolean
+
+    /**
      * Enable blocking, using the previously configured whitelists
      */
     fun enableBlocking()
@@ -16,23 +22,56 @@ interface IBlockTrackersPrivacyModule {
     fun disableBlocking()
 
     /**
-     * Whitelist the App with the specified uid
+     * Set or unset in whitelist the App with the specified uid.
+     * @param appUid the uid of the app
+     * @param isWhiteListed true, the app will appears in whitelist, false, it won't
      */
-    fun unblock(appUid: Int)
+    fun setWhiteListed(appUid: Int, isWhiteListed: Boolean)
 
     /**
-     * Whitelist the specified tracker, for the App specified by its uid.
+     * Set or unset in whitelist the specifid tracked, for the App specified by its uid.
+     * @param tracker the tracker
+     * @param appUid the uid of the app
+     * @param isWhiteListed true, the app will appears in whitelist, false, it won't
      */
-    fun unblock(tracker: Tracker, appUid: Int)
+    fun setWhiteListed(tracker: Tracker, appUid: Int, isWhiteListed: Boolean)
+
+    /**
+     * Return true if nothing has been added to the whitelist : everything is blocked.
+     */
+    fun isWhiteListEmpty(): Boolean
+
+    /**
+     * Return the white listed App, by their UID
+     */
+    fun getWhiteListedApp(): List<Int>
 
     /**
      * Return true if the App is whitelisted for trackers blocking.
      */
     fun isWhitelisted(appUid: Int): Boolean
 
+
     /**
      * List the white listed trackers for an App specified by it uid
      */
     fun getWhiteList(appUid: Int): List<Tracker>
 
+    /**
+     * Callback interface to get updates about the state of the Block trackers module.
+     */
+    interface Listener {
+
+        /**
+         * Called when the trackers blocking is activated or deactivated.
+         * @param isBlocking true when activated, false otherwise.
+         */
+        fun onBlockingToggle(isBlocking: Boolean)
+    }
+
+    fun addListener(listener: Listener)
+
+    fun removeListener(listener: Listener)
+
+    fun clearListeners()
 }
--- privacymodulesapi/src/main/java/foundation/e/privacymodules/trackers/ITrackTrackersPrivacyModule.kt
+++ privacymodulesapi/src/main/java/foundation/e/privacymodules/trackers/ITrackTrackersPrivacyModule.kt
@@ -5,33 +5,33 @@ package foundation.e.privacymodules.trackers
  */
 interface ITrackTrackersPrivacyModule {
 
-
+    /**
+     * List all the trackers encountered for a specific app.
+     */
     fun getTrackersForApp(appUid: Int): List<Tracker>
 
     /**
-     * Return the number of called distincts trackers since "ever"
+     * Return the number of encountered trackers since "ever"
      */
     fun getTrackersCount(): Int
 
     /**
-     * Return the number of called distincts trackers for the last 24 hours
-     */
-    fun getLast24HoursTrackersCount(): Int
-
-    /**
      * Return number of trackers calls by hours, for the last 24hours.
+     * TODO: could be "of the previous day" for a first shot
      * @return list of 24 numbers of trackers calls by hours
      */
     fun getPast24HoursTrackersCalls(): List<Int>
 
     /**
      * Return number of trackers calls by day, for the last 30 days.
+     * TODO: could be "of the previous month" for a first shot
      * @return list of 30 numbers of trackers calls by day
      */
     fun getPastMonthTrackersCalls(): List<Int>
 
     /**
      * Return number of trackers calls by month, for the last 12 month.
+     * TODO: could be "of the previous year" for a first shot
      * @return list of 12 numbers of trackers calls by month
      */
     fun getPastYearTrackersCalls(): List<Int>
